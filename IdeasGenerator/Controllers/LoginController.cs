df5d55a340ea2279f279789862b5476f431fe40d
Metody Aleksov <teddy57603@gmail.com>
2022-09-20 22:22:22 +0300

    added the login method to the login controller

--- IdeasGenerator/Controllers/LoginController.cs
+++ IdeasGenerator/Controllers/LoginController.cs
@@ -1,5 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IdeasGenerator.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
 
 namespace IdeasGenerator.Controllers
 {
@@ -7,6 +15,59 @@ namespace IdeasGenerator.Controllers
     [ApiController]
     public class LoginController : ControllerBase
     {
+        private IConfiguration _config;
 
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Login([FromBody] UserLogin userLogin)
+        {
+            var user = Authenticate(userLogin);
+
+            if(user != null)
+            {
+                var token = Generate(user);
+                return Ok(token);
+            }
+
+            return NotFound("User not found.");
+        }
+
+        private User Authenticate(UserLogin userLogin)
+        {
+            var currentUser = UserConstants.Users.FirstOrDefault(u => u.Username.ToLower() == userLogin.Username.ToLower()
+            && u.Password == userLogin.Password);
+
+            if(currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+        }
+
+        private string Generate(User user)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"]
+                , _config["Jwt:Audience"]
+                , claims
+                , expires: DateTime.Now.AddHours(1)
+                , signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
