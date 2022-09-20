f8df9db3eba799280af9170c8fc2a19dfab734d8
Metody Aleksov <teddy57603@gmail.com>
2022-09-20 21:39:57 +0300

    created user consts

---
+++ IdeasGenerator/Models/UserConstants.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace IdeasGenerator.Models
+{
+    public static class UserConstants
+    {
+        public const List<User> Users = new List<User>()
+        {
+            new User(){ Username = "jsonAdmin", Password = "mypass", Role="admin"},
+            new User(){ Username ="user", Password='mypass2', Role="user"}
+        };
+    }
+}
