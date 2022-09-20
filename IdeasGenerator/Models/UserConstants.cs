ada8541b7e40e8f2444ecc4fc3b274738c602703
Metody Aleksov <teddy57603@gmail.com>
2022-09-20 22:22:32 +0300

    updated user constants

--- IdeasGenerator/Models/UserConstants.cs
+++ IdeasGenerator/Models/UserConstants.cs
@@ -2,12 +2,13 @@
 
 namespace IdeasGenerator.Models
 {
-    public static class UserConstants
+    public class UserConstants
     {
-        public const List<User> Users = new List<User>()
+        public static List<User> Users
+        => new List<User>()
         {
             new User(){ Username = "jsonAdmin", Password = "mypass", Role="admin"},
-            new User(){ Username ="user", Password='mypass2', Role="user"}
+            new User(){ Username ="user", Password="mypass2", Role="user"}
         };
     }
 }
