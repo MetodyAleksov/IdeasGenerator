bdb8127e06f446299600938f4bc39ad639b3015f
Metody Aleksov <teddy57603@gmail.com>
2022-09-21 19:49:33 +0300

    added useractivity model

---
+++ IdeasGenerator.Data/Models/UserActivities.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace IdeasGenerator.Data.Models
+{
+    public class UserActivities
+    {
+        [ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        [ForeignKey(nameof(Activity))]
+        public int ActivityId { get; set; }
+        public Activity Activity { get; set; }
+    }
+}
