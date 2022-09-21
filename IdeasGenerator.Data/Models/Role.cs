f03760e074ea903d7927669a458474c5de74b2fc
Metody Aleksov <teddy57603@gmail.com>
2022-09-21 19:49:09 +0300

    added roles model

---
+++ IdeasGenerator.Data/Models/Role.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace IdeasGenerator.Data.Models
+{
+    public class Role
+    {
+        public Role()
+        {
+            Users = new HashSet<User>();
+        }
+        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public ICollection<User> Users { get; set; }
+    }
+}
