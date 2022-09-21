b1be0c7b0c458b1e45bc7d505cc5fdb1a935a767
Metody Aleksov <teddy57603@gmail.com>
2022-09-21 19:49:19 +0300

    added usersmodel

---
+++ IdeasGenerator.Data/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace IdeasGenerator.Data.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Username { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Password { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(Role))]
+        public int RoleId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public Role Role { get; set; }
+    }
+}
