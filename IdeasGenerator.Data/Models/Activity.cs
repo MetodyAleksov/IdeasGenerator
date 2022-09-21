8f944db5615b3fb78ab67824dc3e4fc025a92fe9
Metody Aleksov <teddy57603@gmail.com>
2022-09-21 19:49:03 +0300

    added activity model

---
+++ IdeasGenerator.Data/Models/Activity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace IdeasGenerator.Data.Models
+{
+    public class Activity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string ActivityDescription { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
