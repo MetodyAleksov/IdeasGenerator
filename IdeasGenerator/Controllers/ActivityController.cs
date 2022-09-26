7350c3faab5adecdd520abba6ccea3186f2f54cf
Metody Aleksov <teddy57603@gmail.com>
2022-09-26 12:59:31 +0300

    succesfully make api call

---
+++ IdeasGenerator/Controllers/ActivityController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace IdeasGenerator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivityController : ControllerBase
+    {
+        [HttpPost("GenerateAction")]
+        [AllowAnonymous]
+        //[Authorize(Roles = "admin")]
+        public async Task<IActionResult> GenerateAction()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri("https://www.boredapi.com/");
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                var response = new HttpResponseMessage();
+
+                response = await client.GetAsync("api/activity");
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = await response.Content.ReadAsStringAsync();
+                    return Ok(result);
+                }
+
+                return NotFound();
+            }
+        }
+    }
+}
